da025a9f1ddc3573c1eb00af3206b3bd810c2bdb
sunjinyuan.3 <sunjinyuan.3@bytedance.com>
2025-12-19 08:27:52 +0800

    fix data and weights path

--- pxdesign/__init__.py
+++ pxdesign/__init__.py
@@ -1,10 +1,12 @@
 import os
+from pathlib import Path
+
+parent_dir = Path(__file__).resolve().parent.parent
 
 if os.environ.get("PROTENIX_DATA_ROOT_DIR") is None:
-    default_value = os.path.abspath("./release_data/ccd_cache")
-    os.environ["PROTENIX_DATA_ROOT_DIR"] = default_value
-    # print(f"Environment variable PROTENIX_DATA_ROOT_DIR not set. Using default: {default_value}")
+    os.environ["PROTENIX_DATA_ROOT_DIR"] = str(
+        parent_dir / "release_data" / "ccd_cache"
+    )
+
 if os.environ.get("TOOL_WEIGHTS_ROOT") is None:
-    default_value = os.path.abspath("./tool_weights")
-    os.environ["TOOL_WEIGHTS_ROOT"] = default_value
-    # print(f"Environment variable TOOL_WEIGHTS_ROOT not set. Using default: {default_value}")
+    os.environ["TOOL_WEIGHTS_ROOT"] = str(parent_dir / "tool_weights")
